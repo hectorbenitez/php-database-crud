dd3c66274f3db76c43c7a53a6fb88abf5236f0e8
Hector Benitez <hector.benitez.fletes@gmail.com>
2018-08-16 13:43:50 -0500

    final version

---
+++ Task.php
@@ -0,0 +1,7 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model {
+    protected $table = 'tasks';
+}
--- index.php
+++ index.php
@@ -5,8 +5,30 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require_once 'vendor/autoload.php';
+require_once 'Task.php';
 
 use Relay\Relay;
+
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+$capsule = new Capsule;
+
+$capsule->addConnection([
+    'driver'    => 'mysql',
+    'host'      => 'localhost',
+    'database'  => 'todo',
+    'username'  => 'root',
+    'password'  => '',
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+]);
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
 
 $request = Zend\Diactoros\ServerRequestFactory::fromGlobals(
     $_SERVER,
@@ -25,31 +47,67 @@ $twig = new \Twig_Environment($loader, array(
 $router = new Aura\Router\RouterContainer();
 $map = $router->getMap();
 $map->get('todo.list', '/', function ($request) use ($twig) {
-    $tasks = [
-        [
-            'id' => 1,
-            'description' => 'Aprender inglés',
-            'done' => false
-        ],
-        [
-            'id' => 1,
-            'description' => 'Hacer la tarea',
-            'done' => true
-        ],
-        [
-            'id' => 1,
-            'description' => 'Pasear al perro',
-            'done' => false
-        ],
-        [
-            'id' => 1,
-            'description' => 'Ver el curso de introducción a PHP',
-            'done' => false
-        ]
-    ];
+    // $tasks = [
+    //     [
+    //         'id' => 1,
+    //         'description' => 'Aprender inglés',
+    //         'done' => false
+    //     ],
+    //     [
+    //         'id' => 1,
+    //         'description' => 'Hacer la tarea',
+    //         'done' => true
+    //     ],
+    //     [
+    //         'id' => 1,
+    //         'description' => 'Pasear al perro',
+    //         'done' => false
+    //     ],
+    //     [
+    //         'id' => 1,
+    //         'description' => 'Ver el curso de introducción a PHP',
+    //         'done' => false
+    //     ]
+    // ];
+    $tasks = Task::all();
     $response = new Zend\Diactoros\Response\HtmlResponse($twig->render('template.twig', [
         'tasks' => $tasks
     ]));
+    return $response;
+});
+
+$map->post('todo.add', '/add', function ($request) {
+    $data = $request->getParsedBody();
+    $task = new Task();
+    $task->description = $data['description'];
+    $task->save();
+    $response = new Zend\Diactoros\Response\RedirectResponse('/');
+    return $response;
+});
+
+$map->get('todo.check', '/check/{id}', function ($request) {
+    $id = $request->getAttribute('id');
+    $task = Task::find($id);
+    $task->done = true;
+    $task->save();
+    $response = new Zend\Diactoros\Response\RedirectResponse('/');
+    return $response;
+});
+
+$map->get('todo.uncheck', '/uncheck/{id}', function ($request) {
+    $id = $request->getAttribute('id');
+    $task = Task::find($id);
+    $task->done = false;
+    $task->save();
+    $response = new Zend\Diactoros\Response\RedirectResponse('/');
+    return $response;
+});
+
+$map->get('todo.delete', '/delete/{id}', function ($request) {
+    $id = $request->getAttribute('id');
+    $task = Task::find($id);
+    $task->delete();
+    $response = new Zend\Diactoros\Response\RedirectResponse('/');
     return $response;
 });
 
