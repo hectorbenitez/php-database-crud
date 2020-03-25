ef6e9240c7fbf4af8d43245e2ad1990d5bc274f9
CristhianCoaquira <cristhiandanielcoaquira@gmail.com>
2020-03-25 12:13:42 -0400

    Adding REST endpoints

--- index.php
+++ index.php
@@ -14,14 +14,14 @@ use Illuminate\Database\Capsule\Manager as Capsule;
 $capsule = new Capsule;
 
 $capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'todo',
-    'username'  => 'root',
-    'password'  => '',
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
+  'driver'    => 'mysql',
+  'host'      => 'localhost',
+  'database'  => 'todo',
+  'username'  => 'root',
+  'password'  => '',
+  'charset'   => 'utf8',
+  'collation' => 'utf8_unicode_ci',
+  'prefix'    => '',
 ]);
 
 // Make this Capsule instance available globally via static methods... (optional)
@@ -31,95 +31,114 @@ $capsule->setAsGlobal();
 $capsule->bootEloquent();
 
 $request = Zend\Diactoros\ServerRequestFactory::fromGlobals(
-    $_SERVER,
-    $_GET,
-    $_POST,
-    $_COOKIE,
-    $_FILES
+  $_SERVER,
+  $_GET,
+  $_POST,
+  $_COOKIE,
+  $_FILES
 );
 
 $loader = new Twig_Loader_Filesystem('.');
 $twig = new \Twig_Environment($loader, array(
-    'debug' => true,
-    'cache' => false,
+  'debug' => true,
+  'cache' => false,
 ));
 
 $router = new Aura\Router\RouterContainer();
 $map = $router->getMap();
 $map->get('todo.list', '/', function ($request) use ($twig) {
-    $tasks = Task::all();
-    $response = new Zend\Diactoros\Response\HtmlResponse($twig->render('template.twig', [
-        'tasks' => $tasks
-    ]));
-    return $response;
+  $tasks = Task::all();
+  $response = new Zend\Diactoros\Response\HtmlResponse($twig->render('template.twig', [
+    'tasks' => $tasks
+  ]));
+  return $response;
 });
 
 $map->post('todo.add', '/add', function ($request) {
-    $data = $request->getParsedBody();
+  $data = $request->getParsedBody();
 
-    $task = new Task();
-    $task->description = $data['description'];
-    $task->save();
+  $task = new Task();
+  $task->description = $data['description'];
+  $task->save();
 
-    $response = new Zend\Diactoros\Response\RedirectResponse('/');
-    return $response;
+  $response = new Zend\Diactoros\Response\RedirectResponse('/');
+  return $response;
 });
 
 $map->get('todo.check', '/check/{id}', function ($request) {
-    $id = $request->getAttribute('id');
+  $id = $request->getAttribute('id');
 
-    $task = Task::find($id);
-    $task->done = true;
-    $task->save();
+  $task = Task::find($id);
+  $task->done = true;
+  $task->save();
 
-    $response = new Zend\Diactoros\Response\RedirectResponse('/');
-    return $response;
+  $response = new Zend\Diactoros\Response\RedirectResponse('/');
+  return $response;
 });
 
 $map->get('todo.uncheck', '/uncheck/{id}', function ($request) {
-    $id = $request->getAttribute('id');
+  $id = $request->getAttribute('id');
 
-    $task = Task::find($id);
-    $task->done = false;
-    $task->save();
+  $task = Task::find($id);
+  $task->done = false;
+  $task->save();
 
-    $response = new Zend\Diactoros\Response\RedirectResponse('/');
-    return $response;
+  $response = new Zend\Diactoros\Response\RedirectResponse('/');
+  return $response;
 });
 
 $map->get('todo.delete', '/delete/{id}', function ($request) {
-    $id = $request->getAttribute('id');
+  $id = $request->getAttribute('id');
 
-    $task = Task::find($id);
-    $task->delete();
+  $task = Task::find($id);
+  $task->delete();
 
-    $response = new Zend\Diactoros\Response\RedirectResponse('/');
-    return $response;
+  $response = new Zend\Diactoros\Response\RedirectResponse('/');
+  return $response;
 });
 
-$map->get('get.tasks', '/tasks', function () {
-    $tasks = Task::all();
-
-    $response = new \Zend\Diactoros\Response\JsonResponse($tasks);
-    return $response;
+$map->get('api.tasks.get', '/tasks', function () {
+  $tasks = Task::all();
+  return new \Zend\Diactoros\Response\JsonResponse($tasks);
 });
 
-$map->post('post.tasks', '/tasks', function ($request) {
-    $data = $request->getBody()->getContents();
-    return var_dump($data);
+$map->post('api.tasks.post', '/tasks', function ($request) {
+  $bodydata = $request->getBody()->getContents();
+  $dataJson = json_decode($bodydata);
+  $task = new Task();
+  $task->description = $dataJson->description;
+  $task->done = false;
+  $task->save();
+  return new \Zend\Diactoros\Response\EmptyResponse(201);
 });
 
+$map->put('api.tasks.put', '/tasks/{id}', function ($request) {
+  $bodydata = $request->getBody()->getContents();
+  $dataJson = json_decode($bodydata);
+  $id = $request->getAttribute('id');
+  $task = Task::find($id);
+  $task->done = $dataJson->done;
+  $task->save();
+  return new \Zend\Diactoros\Response\JsonResponse($task);
+});
+
+$map->delete('api.tasks.delete', '/tasks/{id}', function ($request) {
+  $id = $request->getAttribute('id');
+  $task = Task::find($id);
+  $task->delete();
+  return new \Zend\Diactoros\Response\EmptyResponse();
+});
 
 $relay = new Relay([
-    new Middlewares\AuraRouter($router),
-    new Middlewares\RequestHandler()
+  new Middlewares\AuraRouter($router),
+  new Middlewares\RequestHandler()
 ]);
 
 $response = $relay->handle($request);
 
 foreach ($response->getHeaders() as $name => $values) {
-    foreach ($values as $value) {
-        header(sprintf('%s: %s', $name, $value), false);
-    }
+  foreach ($values as $value) {
+    header(sprintf('%s: %s', $name, $value), false);
+  }
 }
 echo $response->getBody();
